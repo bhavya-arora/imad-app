24e8926def14749e5a1bd01e387eef5d8987aefe
Bhavya Arora <truelover.1995@gmail.com>
2017-08-29 19:44:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='Hello This is Changed Text';
+
+////Lets move the image
+var img=document.getElementById('madi');
+img.onClick=function(){
+    img.style.marginLeft='100px';
+}
