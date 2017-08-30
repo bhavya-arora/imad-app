5a6b1614cca9974d121cbf675ba23b61bbc95c24
Bhavya Arora <truelover.1995@gmail.com>
2017-08-30 15:30:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,17 @@ button.onclick=function(){
     request.send(null);
    
 }
+
+///////submit button
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var list='';
+    var names=['name1','name2','name3','name4'];
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+}
