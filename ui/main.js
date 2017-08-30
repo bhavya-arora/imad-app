babcaf07e8ba532403f64327dc3508ba534d6b66
Bhavya Arora <truelover.1995@gmail.com>
2017-08-30 13:54:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick=function(){
         }
         
     };
-    request.open('GET','http://gobhavyaarora15.imad.hasura-app.io',true);
+    request.open('GET','http://gobhavyaarora15.imad.hasura-app.io/counter',true);
     request.send(null);
    
 }
