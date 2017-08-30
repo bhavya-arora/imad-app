267e83f3046c6c6e00801589c5a0a1734a130d68
Bhavya Arora <truelover.1995@gmail.com>
2017-08-30 13:07:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,13 @@ function moveRight (){
 img.onclick = function(){
    setInterval(moveRight,50);
 }
+
+////////This is for when click me button is clicked//////
+
+var button=document.getElementById('counter');
+var span=document.getElementById('count');
+var counter=0;
+button.onclick=function(){
+    counter = counter+1;
+    span.innerHTML=counter;
+}
