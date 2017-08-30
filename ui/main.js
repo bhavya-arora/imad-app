06252f384576a7304c5a0492532fcf762edaa1e6
Bhavya Arora <truelover.1995@gmail.com>
2017-08-30 14:00:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,14 @@
 
 var button=document.getElementById('counter');
 button.onclick=function(){
+    
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readystate === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter = request.responseText;
                 var span=document.getElementById('count');
-                span.innerHtml=counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
         
