91e96088738ea959460b384dd014899455df83b6
Bhavya Arora <truelover.1995@gmail.com>
2017-08-30 13:12:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,18 @@
+console.log('Loaded!');
 
+var element=document.getElementById('main-text');
+element.innerHTML='Hello This is Changed Text';
+
+////Lets move the image
+var img=document.getElementById('madi');
+var leftMargin=0;
+function moveRight (){
+    leftMargin=leftMargin+5;
+    img.style.marginLeft=leftMargin+'px';
+}
+img.onclick = function(){
+   setInterval(moveRight,50);
+}
 
 ////////This is for when click me button is clicked//////
 
@@ -7,5 +21,5 @@ var span=document.getElementById('count');
 var counter=0;
 button.onclick=function(){
     counter = counter+1;
-    span.innerHTML=counter;
+    span.innerHTML=counter.toString();
 }
