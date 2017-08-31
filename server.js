0410cb67b3f6e830c02635cf84d99873559c45bc
Bhavya Arora <truelover.1995@gmail.com>
2017-08-31 18:00:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,8 +157,8 @@ app.get('/articles/:articleName',function(req,res){
             if(result.rows.length===0){
                 res.status(404).send('Article not found');
             }else{
-                var articleData=result;
-                res.send(articleData);
+                var articleData=result.rows[0];
+                res.send(htmlTemplate(articleData));
             }
         }
     });
