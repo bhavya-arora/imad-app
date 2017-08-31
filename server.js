e874634b6f100ccb9734a788cc863212df3097e5
Bhavya Arora <truelover.1995@gmail.com>
2017-08-31 20:14:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/articles/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
   
 });
