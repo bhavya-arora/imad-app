28cc7f9502fe581830b5e9d6a8fec226eb537287
Bhavya Arora <truelover.1995@gmail.com>
2017-09-01 23:15:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,34 @@ app.post('/create-user',function(req,res){
     });
 });
 
+app.post('/login',function(req,res){
+        app.post('/create-user', function (req, res) {
+       // username, password
+       // {"username": "tanmai", "password": "password"}
+       // JSON
+       var username = req.body.username;
+       var password = req.body.password;
+       pool.query('SELECT FROM  "users" WHERE username=$1', [username], function (err, result) {
+          if (err) {
+              res.status(500).send(err.toString());
+          } else {
+              if(result.rows.lenght===0){
+                  res.send(403).send('Username/password is invalid');
+              }else{
+                  var dbString=result.rows[0].password;
+                  var salt=dbString.split('$')[2];
+                  var hashedValue=hash(password,salt);
+                  if(hashedValue===dbString){
+                      res.send('Credentials are correct');
+                  }else{
+                      res.send(403).send('Username/password is invalid');
+                  }
+              }
+          }
+       });
+    });
+});
+
 
 
 function htmlTemplate (object){
