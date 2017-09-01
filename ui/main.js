504a17702287a3cbd8c7d8af211ffd6ed8393e9c
Bhavya Arora <truelover.1995@gmail.com>
2017-09-01 23:55:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,3 @@
-////////This is for when click me button is clicked//////
-
-var button=document.getElementById('counter');
-button.onclick=function(){
-    
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET','http://gobhavyaarora15.imad.hasura-app.io/counter',true);
-    request.send(null);
-   
-}
-
 ///////submit button
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
@@ -26,21 +5,14 @@ submit.onclick=function(){
     request.onreadystatechange=function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var list='';
-                var names=request.responseText;
-                names=JSON.parse(names);
-                for(var i=0;i<names.length;i++){
-                    list+='<li>'+names[i]+'</li>';
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                alert('You are logged in');
             }
         }
         
     };
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET','http://gobhavyaarora15.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://gobhavyaarora15.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({username:username,password:password}));
    
 };
