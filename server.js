49cea4ee93d0320b0187a25688a7787592610b82
Bhavya Arora <truelover.1995@gmail.com>
2017-09-22 11:44:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/test-db',function(req,res){
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ["pbkdf2","10000",salt,hashed.toString()].join('$');
+    return ["pbkdf2","10000",salt,hashed.toString('binary')].join('$');
     
 }
 
