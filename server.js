c2d67f3f929ccde6a334f36e4e39792e2ec1916d
Bhavya Arora <truelover.1995@gmail.com>
2017-09-29 20:19:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/login', function (req, res) {
                 // internally, on the server side, it maps the session id to an object
                 // { auth: {userId }}
                 req.session.auth = {userId: result.rows[0].id};
-                res.status(200);
+                res.status(200).send();
                 
               } else {
                 res.status(403).send('username/password is invalid');
