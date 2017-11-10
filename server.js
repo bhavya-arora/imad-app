23c2f8d8b6e22c49d279b8d46cd8b27f129063ee
Bhavya Arora <truelover.1995@gmail.com>
2017-11-10 09:13:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/create-user',function(req,res){
     var dbString=hash(password,salt);
     pool.query('INSERT INTO "users" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
-            res.status(500).send(err.toString());
+            res.status(500).send(JSON.stringify({"error":err.toString()}));
         }else{
             res.status(200).send(JSON.stringify({"message":"User Created"}));
         }
